0e6ce9da30b3cffac136e5395040349d2fd25061
Cacha9612 <oswaldoaraelg@gmail.com>
2025-06-14 11:30:47 -0600

    Modificación de modelos y base de datos

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-DATABASE_URL = "mssql+pyodbc://sa:Trotamundosserver@trotamundosdb.cuvrbibw9zda.us-east-2.rds.amazonaws.com/trotamundosdb?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URL = "mssql+pyodbc://sa:Trota01%40@67.205.148.136/trotamundosdb?driver=ODBC+Driver+17+for+SQL+Server"
 engine = create_engine(DATABASE_URL, echo=True)
 engine = engine.execution_options(autocommit = True)
